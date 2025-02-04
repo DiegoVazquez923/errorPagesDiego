1f85ecb8021e6bc4bf518116950f365980b61ce0
DiegoVaz <vazquezsotelodiego@gmail.com>
2025-02-03 21:17:19 -0600

    Cambios

--- Proyectos/errorPages/errorPages/settings.py
+++ Proyectos/errorPages/errorPages/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yd-26zm5ychdft*52*dc+jh91+%lub&-$z*^ps4wkwui4x7-eu'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
